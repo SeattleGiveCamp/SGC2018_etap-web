1e711b06102365a8e4211f98e8ab849e7e22d3da
Ian Jones <ian.reed.jones@gmail.com>
2018-10-27 12:21:53 -0700

    reformat for readability

--- Etap.Web/Controllers/HomeController.cs
+++ Etap.Web/Controllers/HomeController.cs
@@ -11,9 +11,9 @@ namespace Etap.Web.Controllers
         public IActionResult Index()
         {
             return PhysicalFile(
-                Path.Combine(
-                    Directory.GetCurrentDirectory(), "wwwroot/dist/index.html"), "text/html"
-                );
+                Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/dist/index.html"), 
+                "text/html"
+            );
         }
     }
 }
