ba58768c4af684a35976065da5619a9563a6eddc
Will Kuhn <willkuhnblue@gmail.com>
2018-10-27 15:51:17 -0700

    Add deepmerge

--- Etap.Web/src/components/CategoryInfo.js
+++ Etap.Web/src/components/CategoryInfo.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import {withStyles, Typography, Input, TextField} from '@material-ui/core/'
-import { actionCreator } from '../ducks/formData';
+import { setValue } from '../ducks/formData';
 
 const styles = {}
 
@@ -13,14 +13,14 @@ class CategoryInfo extends Component {
     return (
       <Fragment>
         <Typography variant='h3'>whatever</Typography>
-        <TextField label='Leader' value={formData.leader} onChange={(e) => actionCreator(e.target.value, "categories", "category", "group")}/>
+        <TextField label='Leader' value={formData.leader} onChange={(e) => setValue(e.target.value, "categories", "category", "group")}/>
       </Fragment>
     );
   };
 };
 
 const mapStateToProps = state => ({state});
-const mapDispatchToProps = {actionCreator};
+const mapDispatchToProps = {setValue};
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(SiteInfo));
