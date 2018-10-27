b9cb8b6babe5f6eb77991bcf7b8907566b23a5cf
Alex Hong <alexsukhong@gmail.com>
2018-10-27 14:43:33 -0700

    adding file that tabs through forms

--- Etap.Web/src/components/App.js
+++ Etap.Web/src/components/App.js
@@ -6,6 +6,7 @@ import Dashboard from './Dashboard.js';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './Navbar.js';
 import Template from './Template.js';
+import FormContainer from './FieldSummaryReport/FormContainer';
 
 export default class App extends Component {
   render() {
@@ -14,9 +15,9 @@ export default class App extends Component {
         <BrowserRouter>
           <React.Fragment>
             <Route path='/' component={Navbar} />
-            <Route path='/' component={Dashboard} />
+            <Route exact path='/' component={Dashboard} />
             <Route path='/template' component={Template} />
-
+            <Route path='/FieldSummaryReport/FormContainer' component={FormContainer} />
           </React.Fragment>
         </BrowserRouter>
       </Provider>
